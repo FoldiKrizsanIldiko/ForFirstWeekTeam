a002dea6d7cbd86b7160f3ab309c3519c420c63d
FoldiKrizsanIldiko <119942598+FoldiKrizsanIldiko@users.noreply.github.com>
2023-01-05 11:15:40 +0100

    Create basic of our program

---
+++ firstTeamWorkPrep.js
@@ -0,0 +1,137 @@
+let ourTeam={
+    name:"Team Name",
+    members:[
+        {
+        name: "Member1",
+        birthday:{
+             year: 2000,
+             month: 3,
+              day: 1},
+        location:{
+            country: "Hungary",
+            city: "Szeged"},
+        codingLevel: 30,
+        favorites:{
+            favMovies:[{
+                title:'',
+                direcor:"",
+                id:""},
+            {
+                title:'',
+                direcor:"",
+                id:""  
+            }],
+            favBooks:[{
+                    title:"",
+                    writer:"",
+                    year:"",},
+                {
+                    title:"",
+                    writer:"",
+                    year:"",  
+                },
+            ],
+        },
+        colorCombination:{
+            primary:{
+                red:"",
+                green:"",
+                bue:"",
+            },
+            secondary:""
+        },
+    },
+    {
+        name: "Member2",
+        birthday:{
+             year: 2000,
+             month: 3,
+              day: 1},
+        location:{
+            country: "Hungary",
+            city: "Szeged"},
+        codingLevel: 30,
+        favorites:{
+            favMovies:[{
+                title:'',
+                direcor:"",
+                id:""},
+            {
+                title:'',
+                direcor:"",
+                id:""  
+            }],
+            favBooks:[{
+                    title:"",
+                    writer:"",
+                    year:"",},
+                {
+                    title:"",
+                    writer:"",
+                    year:"",  
+                },
+            ],
+        },
+        colorCombination:{
+            primary:{
+                red:"",
+                green:"",
+                bue:"",
+            },
+            secondary:""
+        },
+    },
+        {
+            name: "Member3",
+            birthday:{
+                 year: 2000,
+                 month: 3,
+                  day: 1},
+            location:{
+                country: "Hungary",
+                city: "Szeged"},
+            codingLevel: 30,
+            favorites:{
+                favMovies:[{
+                    title:'',
+                    direcor:"",
+                    id:""},
+                {
+                    title:'',
+                    direcor:"",
+                    id:""  
+                }],
+                favBooks:[{
+                        title:"",
+                        writer:"",
+                        year:"",},
+                    {
+                        title:"",
+                        writer:"",
+                        year:"",  
+                    },
+                ],
+            },
+            colorCombination:{
+                primary:{
+                    red:"",
+                    green:"",
+                    bue:"",
+                },
+                secondary:""
+            },
+        }
+    ]
+}
+
+
+  /*  {
+        name: "Member2",
+        birthday:{year:2001, month:4,day:10 },
+        location:{},// {country: "Hungary",city: "Budapest" },
+        codingLevel: 50
+    }]
+};*/
+
+
+
